682ed7743310870c2335285526e5d86edee393c0
fenetyd <fenety.damtew-ug@aau.edu.et>
2026-02-27 00:04:54 +0300

    solved

---
+++ HackerRank/5_swap-case.py
@@ -0,0 +1,11 @@
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+
+#Time Complexity : O(1)
+#Space Complexity : O(1)
+#time : 5 min
