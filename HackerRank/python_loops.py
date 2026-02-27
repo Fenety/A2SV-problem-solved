82be11bfa5a604ffcbe0404bc5e711f49b4cbf93
fenetyd <fenety.damtew-ug@aau.edu.et>
2026-02-27 03:04:34 +0300

    problem solved

---
+++ HackerRank/python_loops.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i * i)
+
+#Time Complexity : O(n)
+#Space Complexity : O(1)
+#time : 1 min
---
+++ HackerRank/python_power_mod_power.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    m = int(input())
+    
+    print(pow(a, b))
+    print(pow(a, b, m))
+
+#Time Complexity : O(log b)
+#Space Complexity : O(1)
+#time : 3 min
