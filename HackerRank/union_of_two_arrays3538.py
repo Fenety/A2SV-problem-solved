0814d3ae627fe1d3075975726b2a4510d1cd0f8f
fenetyd <fenety.damtew-ug@aau.edu.et>
2026-03-02 20:34:49 +0300

    solutions

---
+++ HackerRank/union_of_two_arrays3538.py
@@ -0,0 +1,9 @@
+class Solution:    
+    def findUnion(self, a, b):
+        ans = a + b
+        
+        return (list(set(ans)))
+    
+#Time Complexity : O(n+m) 
+#Space Complexity : O(n+m) 
+#time : 5 ming
